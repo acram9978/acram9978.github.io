deeb85af3e0e43a2ff3d0521a4eb0bf9735c6fa4
acram9978 <acram2@byui.edu>
2021-05-31 18:47:41 -0600

    week6-windchill

--- Lesson6/javascript/script.js
+++ Lesson6/javascript/script.js
@@ -55,3 +55,17 @@ if (month == 0){
 
 let fullDate = hour + " " + month + " " + day + ", " + year; 
 document.querySelector(".date").textContent = fullDate;
+
+
+var windChill = null;
+var temp = 50;
+var speed = 3.0;
+
+function chillFactor( airTemp, windSpeed ) {
+    var chill = 35.74 + 0.6215 * airTemp - 35.75 * ( windSpeed ** 0.016 ) + 0.4275 * airTemp * ( windSpeed ** 0.16 );
+    return chill;
+}
+
+windChill = chillFactor(temp, speed);
+windChill = windChill.toFixed(2);
+document.querySelector("#chill").textContent = windChill;
