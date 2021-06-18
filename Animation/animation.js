2afb8af174aebe2cb561768a222a01b6583e0da8
acram9978 <acram2@byui.edu>
2021-06-17 21:51:01 -0600

    prophets

---
+++ Animation/animation.js
@@ -0,0 +1,11 @@
+function addClass(){
+    let container = document.querySelector("#first");
+    container.classList.add("container");
+
+    let circle = document.querySelector("#second");
+    circle.classList.add("circle")
+
+    let circle2 = document.querySelector("#third");
+    circle2.classList.add("circle")
+
+}
